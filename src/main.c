a3ff80446977e2b55b7b48f0a825066136ce43d6
Kenji <leokk06@gmail.com>
2021-04-09 16:59:30 -0300

    ✨ Add main

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "parser.h"
+#define N_THREADS 5
+
+int main()
+{
+    BW *image;
+    image = open_pbm("/home/kenji/UNICAMP/MC504/multithread_assignment/data/image_test.pbm");
+
+    printf("altura: %u largura: %u\n", image->size.height, image->size.width);
+
+    for (int i = 0; i < image->size.height; i++)
+    {
+        for (int j = 0; j < image->size.width; j++)
+        {
+            printf("%d ", image->pixels[i][j]);
+        }
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
