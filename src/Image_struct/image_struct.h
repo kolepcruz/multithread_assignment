5031ac632014ea1e687d499c70adf30d2eeeadff
kenji <leokk06@gmail.com>
2021-04-12 17:54:27 -0300

    ✨ Add image utils

--- src/Image_struct/image_struct.h
+++ src/Image_struct/image_struct.h
@@ -1,6 +1,8 @@
 #ifndef __IMG_STRUCT_H__
 #define __IMG_STRUCT_H__
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef uint32_t Channel_type;
 typedef Channel_type **Channel;
@@ -31,5 +33,30 @@ typedef struct RGB
     Channel B;
 
 } RGB;
+
+/**
+ * @brief This function allocates the memory for a black and white image, but doesn't garantee that it will have any default or uniform collor. Should only be used when all pixels will be replaced
+ * 
+ * @param width 
+ * @param height 
+ * @return BW* 
+ */
+BW *new_BW_fast_image(uint32_t width, uint32_t height);
+
+/**
+ * @brief This function allocates the memory for a black and white image, and garantees that it will be a white canvas
+ * 
+ * @param width 
+ * @param height 
+ * @return BW* 
+ */
+BW *new_BW_image(uint32_t width, uint32_t height);
+
+void print_image(BW* image);
+
+void delete_image(BW* image);
+
+void save_image(char *destination, BW *image);
+
 
 #endif //__IMG_STRUCT_H__
